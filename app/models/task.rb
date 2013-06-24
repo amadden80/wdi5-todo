dc6f1b77ea9c4f07d29b8da335f328ad7a8d763f
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-06-24 11:26:47 -0400

    Removing extra routes that havent been made yet

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,4 @@
 class Task < ActiveRecord::Base
-  #We need a migration for this model
+  #We have no controller or migration for this
   attr_accessible :title, :description, :duedate, :is_complete, :address, :priority_id
-
 end
